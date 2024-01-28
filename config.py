4d29881311bda4d3d2d7405a957058f1b04cfa84
andrei_borisevich <andrei_borisevich@epam.com>
2024-01-28 21:04:00 +0300

    init environment

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_API_ID = os.environ["TELEGRAM_API_ID"]
+TELEGRAM_API_HASH = os.environ["TELEGRAM_API_HASH"]
