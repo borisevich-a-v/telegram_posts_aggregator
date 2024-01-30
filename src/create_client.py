d82000d8c9ce09571e5d51ea2a2749900e2f31bf
andrei_borisevich <andrei_borisevich@epam.com>
2024-01-30 18:42:25 +0300

    Move creating to different module

---
+++ src/create_client.py
@@ -0,0 +1,22 @@
+from loguru import logger
+from telethon import TelegramClient, events
+
+from .config import AGGREGATOR_CHANNEL, FUN_CHANNELS, NEWS_CHANNELS, TELEGRAM_API_HASH, TELEGRAM_API_ID
+
+
+def create_client() -> TelegramClient:
+    client = TelegramClient("ChannelParser", TELEGRAM_API_ID, TELEGRAM_API_HASH)
+
+    @client.on(events.NewMessage(chats=FUN_CHANNELS + NEWS_CHANNELS))
+    async def public_channel_listener(event):
+        """
+        This handler just forward messages to the aggregation channel.
+        For some reason bot can't access some posts from other public channel, so we forward posts to the place
+        where bot can access them
+        """
+        await event.client.forward_messages(AGGREGATOR_CHANNEL, event.message)
+        logger.info(f"Got new post {event.message.id} and have added it to the aggregation channel")
+
+    client.start()
+    logger.info("Client has been initialized")
+    return client
