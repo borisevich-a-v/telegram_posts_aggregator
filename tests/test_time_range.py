cb4fa4f9ff934d6724ed7d9cbdfa67f450803140
andrei_borisevich <andrei_borisevich@epam.com>
2024-04-11 17:28:41 +0300

    add Warden and Rule8to11EveryDay and Rule11to12Workdays

--- tests/test_time_range.py
+++ tests/test_time_range.py
@@ -1,5 +1,5 @@
 import pytest
-from pendulum import now, time
+from pendulum import time
 
 from src.warden.time_range import TimeRange
 
@@ -9,7 +9,7 @@ def test_start_equal_end_time():
         TimeRange(time(1), time(1))
 
 
-def test_contains_within_day():
+def test_contains_within_a_day():
     tested_range = TimeRange(time(8), time(19))
 
     assert time(12) in tested_range
