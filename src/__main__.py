b8e457f9188fbf0b80f5c5749230be486986b637
andrei_borisevich <andrei_borisevich@epam.com>
2024-04-13 13:07:37 +0300

    Add src to SOURCE PATH

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,10 @@
 from loguru import logger
 
-from src.craete_bot import create_bot
-from src.create_client import create_client
-from src.posts_storage import PostQueue
-from src.registered_messages import MessagesRegister
-from src.warden.warden import Warden
+from craete_bot import create_bot
+from create_client import create_client
+from posts_storage import PostQueue
+from registered_messages import MessagesRegister
+from warden.warden import Warden
 
 if __name__ == "__main__":
     logger.info("Starting application...")
--- utilities/generate_session.py
+++ utilities/generate_session.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from src.config import TELEGRAM_API_HASH, TELEGRAM_API_ID
+from config import TELEGRAM_API_HASH, TELEGRAM_API_ID
 
 with TelegramClient(StringSession(), TELEGRAM_API_ID, TELEGRAM_API_HASH) as client:
     print(client.session.save())
