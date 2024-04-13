9bcddaa2b785e0f1d4abed5e948985f166250c12
andrei_borisevich <andrei_borisevich@epam.com>
2024-04-13 13:08:15 +0300

    Add RuleLimitAccessInProductiveHours; Refactor warden

--- src/warden/warden.py
+++ src/warden/warden.py
@@ -1,8 +1,9 @@
 import abc
 
-from pendulum import WeekDay, datetime, now, time, today
+from pendulum import DateTime, WeekDay, now, time
 
-from src.warden.time_range import TimeRange
+from config import USER_TIME_ZONE
+from warden.time_range import TimeRange
 
 WORKING_DAYS = WeekDay.MONDAY, WeekDay.TUESDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY
 
@@ -15,47 +16,70 @@ class IRule(abc.ABC):
     """Raise an `NotAllowed` exception in case of check has failed"""
 
     @abc.abstractmethod
-    def check(self, checked_time: datetime) -> None: ...
+    def check(self, current: DateTime) -> None: ...
 
 
 class Rule8to11EveryDay(IRule):
     FORBIDDEN_TIME = TimeRange(time(8), time(11))
 
-    def check(self, checked_time: datetime) -> None:
-        if checked_time in self.FORBIDDEN_TIME:
+    def check(self, current: DateTime) -> None:
+        if current.time() in self.FORBIDDEN_TIME:
             raise NotAllowed("The rule does not allow you to ask for posts. (8am-11am every day)")
 
 
 class Rule11to12Workdays(IRule):
-    FORBIDDEN_TIME: TimeRange = TimeRange(time(11), time(12))
+    FORBIDDEN_TIME = TimeRange(time(11), time(12))
     FORBIDDEN_DAYS: tuple[WeekDay, ...] = WORKING_DAYS
 
-    def check(self, checked_time: datetime) -> None:
-        if today().day_of_week in self.FORBIDDEN_DAYS:
-            if checked_time in self.FORBIDDEN_TIME:
+    def check(self, current: DateTime) -> None:
+        if current.day_of_week in self.FORBIDDEN_DAYS:
+            if current.time() in self.FORBIDDEN_TIME:
                 raise NotAllowed("The rule does not allow you to ask for posts. (11am-12pm workdays)")
 
 
 class RuleSleepTimeMonTue(IRule):
     # To be implemented
-    def check(self, checked_time: datetime) -> None: ...
+    def check(self, current: DateTime) -> None: ...
 
 
-class RuleLimitAccessInWorkingHours(IRule):
-    # To be implemented
-    def check(self, checked_time: datetime) -> None: ...
+class RuleLimitAccessInProductiveHours(IRule):
+    RESTRICTED_TIME = TimeRange(time(12), time(18))
+
+    # No more than 20 posts in 30 minutes
+    ALLOWED_POSTS_NUMBER = 20
+    TIME_PERIOD = 30 * 60  # 30 minutes
+
+    def __init__(self):
+        self.last_access_times: list[DateTime] = []
+
+    def _remove_old_accesses(self, current: DateTime):
+        old_times = self.last_access_times
+        self.last_access_times = []
+        for access_time in old_times:
+            if (current.int_timestamp - access_time.int_timestamp) < self.TIME_PERIOD:
+                self.last_access_times.append(access_time)
+
+    def check(self, current: DateTime) -> None:
+        if current.time() not in self.RESTRICTED_TIME:
+            return
+
+        self._remove_old_accesses(current)
+        self.last_access_times.append(current)
+        if len(self.last_access_times) > self.ALLOWED_POSTS_NUMBER:
+            raise NotAllowed("You are asking for new posts too often. (12pm-18pm mo more than 20 posts in 30 min)")
 
 
 class Warden:
     RULES = [
         Rule8to11EveryDay,
         Rule11to12Workdays,
+        RuleLimitAccessInProductiveHours,
     ]
 
     def __init__(self):
         self._rules: list[IRule] = [rule() for rule in self.RULES]
 
     def check_allowance(self) -> None:
-        current_time = now().time()
+        current_datetime = now(tz=USER_TIME_ZONE)
         for rule in self._rules:
-            rule.check(current_time)
+            rule.check(current_datetime)
