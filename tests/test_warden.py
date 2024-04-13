00ebdb996daf9deafbee0a5953b0dd5b60ee40ca
andrei_borisevich <andrei_borisevich@epam.com>
2024-04-13 13:08:15 +0300

    Add RuleLimitAccessInProductiveHours; Refactor warden

--- tests/test_warden.py
+++ tests/test_warden.py
@@ -1,15 +1,20 @@
 from unittest.mock import Mock
 
 import pytest
-from pendulum import time
+from pendulum import DateTime, Timezone
 
-from src.warden.warden import NotAllowed, Rule8to11EveryDay, Rule11to12Workdays, Warden
+from warden.warden import NotAllowed, Rule8to11EveryDay, Rule11to12Workdays, RuleLimitAccessInProductiveHours, Warden
+
+
+def get_datetime_with_specific_time(hour, minute=0, second=0) -> DateTime:
+    return DateTime(year=2024, month=1, day=1, hour=hour, minute=minute, second=second, tzinfo=Timezone("UTC"))
 
 
 def test_warden_rules_list():
     expected_rules = {
         Rule8to11EveryDay,
         Rule11to12Workdays,
+        RuleLimitAccessInProductiveHours,
     }
     assert expected_rules == set(Warden.RULES)
 
@@ -40,35 +45,100 @@ def test_rule_raised_an_exception():
 def test_rule_8_to_11_every_day_positive():
     """Check for Rule8to11EveryDay"""
     rule = Rule8to11EveryDay()
-    rule.check(time(hour=11, second=1))
-    rule.check(time(hour=12))
-    rule.check(time(hour=23))
-    rule.check(time(hour=7, minute=59))
+    rule.check(get_datetime_with_specific_time(hour=11, second=1))
+    rule.check(get_datetime_with_specific_time(hour=12))
+    rule.check(get_datetime_with_specific_time(hour=23))
+    rule.check(get_datetime_with_specific_time(hour=7, minute=59))
 
 
 def test_rule_8_to_11_every_day_negative():
     """Check for Rule8to11EveryDay"""
     rule = Rule8to11EveryDay()
     with pytest.raises(NotAllowed):
-        rule.check(time(hour=8, second=1))
+        rule.check(get_datetime_with_specific_time(hour=8, second=1))
 
     with pytest.raises(NotAllowed):
-        rule.check(time(hour=10, minute=59))
+        rule.check(get_datetime_with_specific_time(hour=10, minute=59))
 
 
-def test_rule_11_to_12_weekdays_positive():
-    """Check for Rule11to12Weekdays"""
+def test_rule_11_to_12_workdays_positive_workday():
+    """Check for Rule11to12Workdays"""
     rule = Rule11to12Workdays()
-    rule.check(time(hour=12, second=1))
-    rule.check(time(hour=23))
-    rule.check(time(hour=10, minute=59))
+    rule.check(DateTime(year=2024, month=4, day=11, hour=12, second=1))
+    rule.check(DateTime(year=2024, month=4, day=11, hour=23))
+    rule.check(DateTime(year=2024, month=4, day=11, hour=10, second=59))
 
 
-def test_rule_11_to_12_weekdays_negative():
-    """Check for Rule11to12Weekdays"""
+@pytest.mark.parametrize("day", [13, 14])  # sat and sun
+def test_rule_11_to_12_workdays_positive_weekend(day):
+    """Check for Rule11to12Workdays"""
+    rule = Rule11to12Workdays()
+    rule.check(DateTime(year=2024, month=4, day=day, hour=11, minute=30))
+
+
+@pytest.mark.parametrize("day", [8, 9, 10, 11, 12])  # mon-fri
+def test_rule_11_to_12_workdays_negative_workdays(day):
+    """Check for Rule11to12Workdays"""
     rule = Rule11to12Workdays()
     with pytest.raises(NotAllowed):
-        rule.check(time(hour=11, second=1))
+        rule.check(DateTime(year=2024, month=4, day=day, hour=11, minute=30))
+
+
+def test_rule_11_to_12_workdays_negative():
+    """Check for Rule11to12Workdays"""
+    rule = Rule11to12Workdays()
+    with pytest.raises(NotAllowed):
+        rule.check(DateTime(year=2024, month=4, day=11, hour=11, minute=1))
 
     with pytest.raises(NotAllowed):
-        rule.check(time(hour=11, minute=59))
+        rule.check(DateTime(year=2024, month=4, day=11, hour=11, minute=59))
+
+
+def test_rule_rule_limit_access_in_working_hours_few_access():
+    """Check for RuleLimitAccessInProductiveHours"""
+    rule = RuleLimitAccessInProductiveHours()
+    allowed_posts_number = 20
+    for i in range(allowed_posts_number):
+        rule.check(get_datetime_with_specific_time(hour=12, minute=i + 1))
+
+
+def test_rule_rule_limit_access_in_working_hours_big_delay():
+    """Check for RuleLimitAccessInProductiveHours"""
+    rule = RuleLimitAccessInProductiveHours()
+    allowed_posts_number = 20
+    time_period_minutes = 30
+
+    for i in range(allowed_posts_number):
+        rule.check(get_datetime_with_specific_time(hour=12, second=i))
+
+    for i in range(allowed_posts_number):
+        rule.check(
+            get_datetime_with_specific_time(
+                hour=12, minute=time_period_minutes + 20, second=i + len(rule.last_access_times)
+            )
+        )
+
+
+def test_rule_rule_limit_access_in_working_hours_several_small_delays():
+    """Check for RuleLimitAccessInProductiveHours"""
+    rule = RuleLimitAccessInProductiveHours()
+    allowed_posts_number = 20
+    time_period_minutes = 30
+
+    for i in range(allowed_posts_number // 2):
+        rule.check(get_datetime_with_specific_time(hour=12, second=i))
+
+    for i in range(allowed_posts_number // 2):
+        rule.check(get_datetime_with_specific_time(hour=12, minute=time_period_minutes // 2 + 1, second=i + 1))
+
+    for i in range(allowed_posts_number // 2):
+        rule.check(get_datetime_with_specific_time(hour=12, minute=time_period_minutes, second=i + 2))
+
+
+def test_rule_rule_limit_access_in_working_hours_negative():
+    """Check for RuleLimitAccessInProductiveHours"""
+    rule = RuleLimitAccessInProductiveHours()
+    allowed_posts_number = 20
+    with pytest.raises(NotAllowed):
+        for i in range(allowed_posts_number + 1):
+            rule.check(get_datetime_with_specific_time(hour=12, second=i + 1))
