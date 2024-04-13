e78aa6220f6c356ce0e9ce2576a6fb58a768084b
andrei_borisevich <andrei_borisevich@epam.com>
2024-04-13 14:41:58 +0300

    Add default values for config

--- src/config.py
+++ src/config.py
@@ -9,20 +9,20 @@ LIST_DELIMITER = "|"
 
 
 def load_env_list(env_name: str) -> list[str]:
-    return os.environ[env_name].split(LIST_DELIMITER)
+    return os.environ.get(env_name, "").split(LIST_DELIMITER)
 
 
-TELEGRAM_BOT_TOKEN = str(os.environ["TELEGRAM_BOT_TOKEN"])
-TELEGRAM_API_ID = str(os.environ["TELEGRAM_API_ID"])
-TELEGRAM_API_HASH = str(os.environ["TELEGRAM_API_HASH"])
+TELEGRAM_BOT_TOKEN = str(os.environ.get("TELEGRAM_BOT_TOKEN"))
+TELEGRAM_API_ID = str(os.environ.get("TELEGRAM_API_ID"))
+TELEGRAM_API_HASH = str(os.environ.get("TELEGRAM_API_HASH"))
 
-CLIENT_SESSION = str(os.environ["CLIENT_SESSION"])
+CLIENT_SESSION = str(os.environ.get("CLIENT_SESSION"))
 
-ADMIN = int(os.environ["ADMIN"])
+ADMIN = int(os.environ.get("ADMIN", 0))
 
 FUN_CHANNELS = load_env_list("FUN_CHANNELS")
 NEWS_CHANNELS = load_env_list("NEWS_CHANNELS")
 
-AGGREGATOR_CHANNEL = str(os.environ["AGGREGATOR_CHANNEL"])
+AGGREGATOR_CHANNEL = str(os.environ.get("AGGREGATOR_CHANNEL"))
 
-USER_TIME_ZONE = pendulum.timezone(str(os.environ["TIMEZONE"]))  # The developer is the  only user of this bot
+USER_TIME_ZONE = pendulum.timezone(str(os.environ.get("TIMEZONE", "UTC")))  # The developer is the only user
