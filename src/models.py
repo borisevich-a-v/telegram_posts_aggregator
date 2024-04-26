07d125fc6143a55e3f8f0aebf81c98f1be753173
andrei_borisevich <andrei_borisevich@epam.com>
2024-04-26 11:56:46 +0300

    Add alembic for migrations control

---
+++ src/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class MessageModel(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True)
+    message_id = Column(Integer, nullable=False)
+    grouped_id = Column(Integer, nullable=True)
+    sent = Column(DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"MessageModel({self.id, self.message_id, self.grouped_id, self.sent})"
