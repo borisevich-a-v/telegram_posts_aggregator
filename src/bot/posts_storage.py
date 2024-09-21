053079bd762375952f2c861f616823a9339df8cc
borisevich-a-v <61206636+borisevich-a-v@users.noreply.github.com>
2024-09-21 21:00:55 +0300

    Deduplicate forwarded messages (#29)

--- src/bot/posts_storage.py
+++ src/bot/posts_storage.py
@@ -2,15 +2,31 @@ from datetime import datetime
 
 from loguru import logger
 from sqlalchemy import func
-from telethon.tl.types import Message
+from sqlalchemy.orm import Session
+from telethon.tl.types import Message, PeerChannel
 
 from models import ChannelModel, MessageModel
 
 MESSAGE_ID = int
 
 
+def get_peer_id(message: Message) -> int:
+    if isinstance(message.fwd_from.from_id, PeerChannel):
+        return message.fwd_from.from_id.channel_id
+    # elif isinstance(message.fwd_from.from_id, PeerChat):
+    #     return message.fwd_from.from_id.chat_id
+    # elif isinstance(message.fwd_from.from_id, PeerUser):
+    #     return message.fwd_from.from_id.user_id
+
+    raise ValueError("Not a channel. Probably spam.")
+
+
 class NoNewPosts(Exception):
     """Post storage raise this exception when there are no new posts"""
+
+
+class PostDuplication(Exception):
+    """This post has been saved into database previously, probably it's a repost"""
 
 
 class PostStorage:
@@ -20,8 +36,16 @@ class PostStorage:
 
     def post(self, message: Message) -> None:
         with self.session_maker() as session:
-            channel = self.get_or_create_channel(message.fwd_from.from_id.channel_id, session)
-            message = MessageModel(message_id=message.id, grouped_id=message.grouped_id, channel=channel)
+            original_channel_id = get_peer_id(message)
+            original_message_id = message.fwd_from.channel_post
+            self.validate_duplication(original_message_id, original_channel_id, session)
+
+            message = MessageModel(
+                message_id=message.id,
+                grouped_id=message.grouped_id,
+                channel=self.get_or_create_channel(original_channel_id, session),
+                original_message_id=original_message_id,
+            )
             session.add(message)
             session.commit()
 
@@ -33,6 +57,14 @@ class PostStorage:
             session.add(channel)
             session.commit()
         return channel
+
+    def validate_duplication(self, original_message_id: int, channel_id: int, session: Session) -> None:
+        if (
+            session.query(MessageModel)
+            .filter(MessageModel.original_message_id == original_message_id, MessageModel.channel_id == channel_id)
+            .first()
+        ):
+            raise PostDuplication(f"Same post from {channel_id}")
 
     def get_oldest_unsent_post(self) -> list[MESSAGE_ID]:
         # todo: should we use transactions here??
