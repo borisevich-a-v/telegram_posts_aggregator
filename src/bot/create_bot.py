543f9fa7e328629121cb3de5328c1e4bc9d8be86
Andrei Borisevich <andreyborisevichleti@gmail.com>
2024-10-13 10:33:25 +0300

    Dynamic channel requests; refactor create_bot.py

--- src/bot/create_bot.py
+++ src/bot/create_bot.py
@@ -1,12 +1,45 @@
+import re
+
 from loguru import logger
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
+from typing_extensions import NamedTuple
 
 from bot.warden.warden import NotAllowed, Warden
 from config import ADMIN, AGGREGATOR_CHANNEL, TELEGRAM_API_HASH, TELEGRAM_API_ID, TELEGRAM_BOT_TOKEN
-from models import ChannelType
 from posts_storage import NoNewPosts, PostStorage
 from telegram_slow_client import TelegramSlowClient
+
+ANY_CHANNEL_COMMAND = "next"
+
+
+class PostRequestError(Exception):
+    """This error represents any problem with new posts request"""
+
+
+class PostRequest(NamedTuple):
+    channel_type: str | None
+    amount: int
+
+    @classmethod
+    def from_event(cls, event):
+        channel_type = event.pattern_match.group(1)
+        if channel_type is None:
+            raise PostRequestError("Bad request format. Can't parse requested channel type")
+        if channel_type == ANY_CHANNEL_COMMAND:
+            channel_type = None
+
+        amount = int(event.pattern_match.group(2) or "1")
+        if amount > 100:
+            amount = 100
+
+        return cls(channel_type, amount)
+
+
+def get_request_pattern(post_storage: PostStorage) -> re.Pattern:
+    type_pattern = f"({'|'.join(t for t in (post_storage.get_all_custom_channel_types() + [ANY_CHANNEL_COMMAND]))})"
+    amount_pattern = rf"(\d{{0,5}})"
+    return re.compile(rf"/{type_pattern}{amount_pattern}")
 
 
 def create_bot(post_storage: PostStorage, warden: Warden) -> TelegramClient:
@@ -15,6 +48,7 @@ def create_bot(post_storage: PostStorage, warden: Warden) -> TelegramClient:
         bot_token=TELEGRAM_BOT_TOKEN
     )
 
+    # TODO: should we parse album here?
     @bot.on(events.NewMessage(chats=AGGREGATOR_CHANNEL))
     async def aggregator_channel_listener(event) -> None:
         if hasattr(event, "message"):
@@ -22,34 +56,26 @@ def create_bot(post_storage: PostStorage, warden: Warden) -> TelegramClient:
         else:
             logger.critical("No message {}", event)
 
-    @bot.on(events.NewMessage(pattern=rf"/({'|'.join(t for t in ChannelType)})(\d{{0,5}})", from_users=ADMIN))
+    @bot.on(events.NewMessage(pattern=get_request_pattern(post_storage), from_users=ADMIN))
     async def handle_posts_request_command(event) -> None:
         logger.info(f"Got {event.pattern_match} request from Admin")
 
         try:
-            channel_type = ChannelType(event.pattern_match.group(1))
-            if channel_type is ChannelType.ALL:
-                channel_type = None
-        except (ValueError, IndexError) as exp:
-            await event.reply(f"Bad request format: {exp}")
+            request = PostRequest.from_event(event)
+        except PostRequestError as exp:
+            await event.reply(str(exp))
             return
 
-        amount = int(event.pattern_match.group(2) or "1")
-        if amount > 100:
-            amount = 100
-            await event.reply("Can't send more than 100 posts in one shot")
-
-        for i in range(amount):
-
+        for _ in range(request.amount):
             try:
                 warden.check_allowance()
+                pass
             except NotAllowed as exp:
-                logger.info("User is not allowed to read a new message %s", str(exp))
                 await event.reply(str(exp))
                 return
 
             try:
-                msgs = post_storage.get_oldest_unsent_post(channel_type)
+                msgs = post_storage.get_oldest_unsent_post(request.channel_type)
                 await event.client.forward_messages(entity=ADMIN, messages=msgs, from_peer=AGGREGATOR_CHANNEL)
                 post_storage.set_sent_multiple(msgs)
             except NoNewPosts:
---
+++ tests/test_create_bot.py
@@ -0,0 +1,43 @@
+import re
+from unittest.mock import Mock
+
+import pytest
+
+from bot.create_bot import PostRequest, PostRequestError, get_request_pattern
+
+
+def test_get_request_pattern():
+    post_storage = Mock()
+    post_storage.get_all_custom_channel_types = Mock(return_value=["type1", "type2"])
+
+    res = get_request_pattern(post_storage)
+
+    expected = re.compile(r"/(type1|type2|next)(\d{0,5})")
+
+    assert res == expected
+
+
+@pytest.mark.parametrize(
+    ("type_", "amount", "expected"),
+    (
+        ("type1", "50", PostRequest("type1", 50)),
+        ("type2", "104", PostRequest("type2", 100)),
+        ("next", "1", PostRequest(None, 1)),
+        ("type3", None, PostRequest("type3", 1)),
+    ),
+)
+def test_post_request(type_, amount, expected):
+    event = Mock()
+    event.pattern_match.group = Mock(side_effect=lambda x: {1: type_, 2: amount}[x])
+
+    res = PostRequest.from_event(event)
+
+    assert res == expected
+
+
+def test_post_request_bad_format():
+    event = Mock()
+    event.pattern_match.group = Mock(side_effect=lambda x: {1: None, 2: 10}[x])
+
+    with pytest.raises(PostRequestError):
+        PostRequest.from_event(event)
